99ab59b34fc376c69b5c307510973e37a2ef0832
sumhaxer <61248925+sumhaxer@users.noreply.github.com>
2021-02-11 20:55:34 +0100

    Update updater.py

--- updater.py
+++ updater.py
@@ -1,6 +1,11 @@
 from urllib.request import urlopen
 
-with open("main.py", "w") as f:
-	data = urlopen("https://raw.githubusercontent.com/sumhaxer/password-manager/main/main.py")
-	f.write(data.read().decode())
-	f.close()
+def main():
+
+	with open("main.py", "w") as f:
+		data = urlopen("https://raw.githubusercontent.com/sumhaxer/password-manager/main/main.py")
+		f.write(data.read().decode())
+		f.close()
+
+if __name__ == "__main__":
+	main()
